21479b8a88cec6cd8810682dcd0c1288e1a656a7
dipanshu0991 <deepss507@gmail.com>
2019-03-05 17:23:43 +0530

    login validation

--- src/app/routingcomponents/dashboard-maker/dashboard-maker.component.ts
+++ src/app/routingcomponents/dashboard-maker/dashboard-maker.component.ts
@@ -13,51 +13,36 @@ export class DashboardMakerComponent implements OnInit {
   billdata:any=[];
   pendingbillers:any=[];
   billerlength:number=0;
-approvedcard:any=[
-  {approvedby: "Mr. K.V. HEBBAR",
+  approvedcard:any=[
+    {approvedby: "Mr. K.V. HEBBAR",
+    aproveddate: "28-02-2019",
+    aprovedtime: "12:10:52",
+    cardholder: "Test card",
+    digits: "4859 xxxx xxxx 0005",
+    expirydate: "09/22",
+    id: 1,
+    initiatedby: "Mr. Naveen Lohiya",
+    initiateddate: "28-02-2019",
+    initiatedtime: "12:09:38",
+    orgid: 73,
+    regcmt: "",
+    status: 1},
+    {
+      approvedby: "Mr. K.V. HEBBAR",
   aproveddate: "28-02-2019",
-  aprovedtime: "12:10:52",
-  cardholder: "Qqwqwqw",
-  digits: "3354xxxx xxxx8553",
-  expirydate: "04/21",
-  id: 203,
+  aprovedtime: "19:17:24",
+  cardholder: "Test Card 1",
+  digits: "4859 xxxx xxxx 0047",
+  expirydate: "06/22",
+  id: 2,
   initiatedby: "Mr. Naveen Lohiya",
   initiateddate: "28-02-2019",
-  initiatedtime: "12:09:38",
+  initiatedtime: "17:30:29",
   orgid: 73,
   regcmt: "",
-  status: 1},
-  {
-    approvedby: "Mr. K.V. HEBBAR",
-aproveddate: "28-02-2019",
-aprovedtime: "19:17:24",
-cardholder: "Anurag",
-digits: "6777xxxx xxxx6879",
-expirydate: "06/21",
-id: 205,
-initiatedby: "Mr. Naveen Lohiya",
-initiateddate: "28-02-2019",
-initiatedtime: "17:30:29",
-orgid: 73,
-regcmt: "",
-status: 1
-  },
-  {
-    approvedby: "Mr. K.V. HEBBAR",
-aproveddate: "28-02-2019",
-aprovedtime: "19:20:40",
-cardholder: "Annu",
-digits: "9865xxxx xxxx7909",
-expirydate: "06/24",
-id: 206,
-initiatedby: "Mr. Naveen Lohiya",
-initiateddate: "28-02-2019",
-initiatedtime: "17:30:42",
-orgid: 73,
-regcmt: "",
-status: 1
-  }
-]
+  status: 1
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
