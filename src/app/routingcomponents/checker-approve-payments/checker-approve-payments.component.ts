16b4285edc3381f4247e818e13f3798b4b0a9956
S Ravi Kumar <dronzer@Ss-MacBook-Pro.local>
2019-03-05 17:22:40 +0530

    checker payment

--- src/app/routingcomponents/checker-approve-payments/checker-approve-payments.component.ts
+++ src/app/routingcomponents/checker-approve-payments/checker-approve-payments.component.ts
@@ -12,9 +12,29 @@ export class CheckerApprovePaymentsComponent implements OnInit {
   displaydownloadlist='none';
   displayreason='none';
   displaypaydetails='none';
+  filterQuery = "";
+  rowsOnPage = 300;
+  sortBy = "email";
+  sortOrder = "asc";
+  billerlength:number=0;
+  noofrole="No billers available"
+  payments:any=[]
+  pendingPayments:any = []
+  public checkedValueArray: any = [];
+  selectall:boolean=false;
+  public temp: any;
+  public cntChk: any;
+  public flag: any;
+  select=false;
+
   constructor() { }
 
   ngOnInit() {
+    this.payments=JSON.parse(localStorage.getItem('payments'));
+    this.pendingPayments = this.payments.filter((payment)=>{
+      return (payment.status == "Pending")
+    })
+
   }
   openModalDialog1(){
     this.displaydownloadlist='';  //Set block css
@@ -38,6 +58,17 @@ export class CheckerApprovePaymentsComponent implements OnInit {
   this.pendingList=false;
   this.approve=true;
   this.reject=false;
+
+
+  for(var i=0;i<this.checkedValueArray.length;i++){
+    for(var j=0;j<this.payments.length;j++){
+      if(this.checkedValueArray[i] ==  this.payments[j].id){
+        this.payments[j].status ="Approved"
+      }
+    }
+
+    localStorage.setItem('payments' , JSON.stringify(this.payments));
+  }
 }
 rejectBtn(){
   this.pendingList=false;
@@ -49,4 +80,74 @@ viewBtn(){
   this.approve=false;
   this.reject=false;
 }
+
+
+
+
+
+changeAll(pendingbillerpage): void {
+    
+  if(this.checkedValueArray.length==this.payments.length){
+  this.cntChk=1
+  }else{
+  this.checkedValueArray = [];
+  this.cntChk=0
+  }
+  console.log(this.selectall)
+  if (this.cntChk == 0) {
+    this.cntChk = 1;
+    this.temp = true;
+    this.selectall=true;
+    this.select=true;
+    for (var i = 0; i < pendingbillerpage.length; i++) {
+      this.checkedValueArray[i] = pendingbillerpage[i].id;
+    }
+    this.cntChk = 0;
+  }
+ 
+  else {
+    this.cntChk = 0;
+    this.temp = false;
+    this.checkedValueArray = [];
+    this.select=false;
+  }
+  console.log(this.checkedValueArray)
+}
+
+change(id): void {
+  this.flag = 0;
+  for (var i = 0; i < this.checkedValueArray.length; i++) {
+    if (this.checkedValueArray[i] == id) {
+      this.checkedValueArray.splice(i, 1);
+      this.flag = 1;
+    }
+  }
+  if (this.flag == 0) {
+    this.checkedValueArray.push(id);
+   
+  }
+
+  if (this.checkedValueArray.length > 0) {
+    this.temp = true;
+    if(this.checkedValueArray.length<this.payments.length){
+      this.selectall=false
+    }else{
+      this.selectall=true;
+      this.cntChk = 1;
+    }
+    console.log(this.selectall)
+   
+  }
+  else {
+    this.temp = false;
+    if(this.checkedValueArray.length<this.payments.length){
+      this.selectall=false
+    }else{
+      this.selectall=true;
+    }
+    console.log(this.selectall)
+   
+  }
+  console.log(this.checkedValueArray)
+}  
 }
