0c1a3f100d69126be536d7058603a76de3249ce9
dipanshu0991 <deepss507@gmail.com>
2019-03-06 13:42:14 +0530

    Bill pay count change in dashboard

--- src/app/routingcomponents/dashboard-checker/dashboard-checker.component.ts
+++ src/app/routingcomponents/dashboard-checker/dashboard-checker.component.ts
@@ -12,8 +12,19 @@ export class DashboardCheckerComponent implements OnInit {
   @ViewChild('owlElement') owlElement: OwlCarousel
   carddata:any;
   billdata:any=[];
+  paymentdata:any=[];
+  penPaydata:any=[];
+  apprPaydata:any=[];
+  rejPayData:any=[];
   pendingbillers:any=[];
+ apprbillers:any=[];
+ rejbillers:any=[];
   billerlength:number=0;
+  apprbilllength:number=0;
+  rejbilllength:number=0
+  penpaylength:number=0;
+  apprpaylength:number=0;
+  rejpaylength:number=0
   public currentCard: any=0;
   approvedcard:any=[
     {approvedby: "Mr. K.V. HEBBAR",
@@ -51,16 +62,59 @@ export class DashboardCheckerComponent implements OnInit {
     this.httpService.get('./assets/cards.json').subscribe(data=>{
       this.carddata=data;
       console.log(this.carddata)
+     
       this.billdata=JSON.parse(localStorage.getItem('billdetails'));
+      this.paymentdata=JSON.parse(localStorage.getItem('payments'));
       console.log(this.billdata)
+      if(this.billdata!=null){
       this.pendingbillers = this.billdata.filter((data)=>data['status']=='Pending');
+      this.apprbillers = this.billdata.filter((data)=>data['status']=='Approved');
+      this.rejbillers = this.billdata.filter((data)=>data['status']=='Rejected');
       if(this.pendingbillers==null){
         this.billerlength=0
       
       }else{
         this.billerlength=this.pendingbillers.length;
-        
       }
+      if(this.apprbillers==null){
+        this.apprbilllength=0
+      }else{
+        this.apprbilllength=this.apprbillers.length;
+      }
+      if(this.rejbillers==null){
+        this.rejbilllength=0
+      }else{
+        this.rejbilllength=this.rejbillers.length;
+      }
+    }else{
+      this.billerlength=0
+      this.apprbilllength=0
+      this.rejbilllength=0
+    }
+    if(this.paymentdata!=null){
+      this.penPaydata = this.paymentdata.filter((data)=>data['status']=='Pending');
+      this.apprPaydata = this.paymentdata.filter((data)=>data['status']=='Approved');
+      this.rejPayData = this.paymentdata.filter((data)=>data['status']=='Rejected');
+      if(this.penPaydata==null){
+        this.penpaylength=0
+      }else{
+        this.penpaylength=this.penPaydata.length; 
+      }
+      if(this.apprPaydata==null){
+        this.apprpaylength=0
+      }else{
+        this.apprpaylength=this.apprPaydata.length; 
+      }
+      if(this.rejPayData==null){
+        this.rejpaylength=0
+      }else{
+        this.rejpaylength=this.rejPayData.length; 
+      }
+    }else{
+  this.penpaylength=0
+  this.apprpaylength=0
+  this.rejpaylength=0
+    }
     })
   }
   fun() {
--- src/app/routingcomponents/dashboard-maker/dashboard-maker.component.ts
+++ src/app/routingcomponents/dashboard-maker/dashboard-maker.component.ts
@@ -11,8 +11,19 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 export class DashboardMakerComponent implements OnInit {
   public currentCard: any=0;
   billdata:any=[];
+  paymentdata:any=[];
+  penPaydata:any=[];
+  apprPaydata:any=[];
+  rejPayData:any=[];
   pendingbillers:any=[];
+  apprbillers:any=[];
+ rejbillers:any=[];
   billerlength:number=0;
+  apprbilllength:number=0;
+  rejbilllength:number=0
+  penpaylength:number=0;
+  apprpaylength:number=0;
+  rejpaylength:number=0
   approvedcard:any=[
     {approvedby: "Mr. K.V. HEBBAR",
     aproveddate: "28-02-2019",
@@ -47,9 +58,12 @@ export class DashboardMakerComponent implements OnInit {
 
   ngOnInit() {
     this.billdata=JSON.parse(localStorage.getItem('billdetails'));
+    this.paymentdata=JSON.parse(localStorage.getItem('payments'));
     console.log(this.billdata)
     if(this.billdata!=null){
     this.pendingbillers = this.billdata.filter((data)=>data['status']=='Pending');
+    this.apprbillers = this.billdata.filter((data)=>data['status']=='Approved');
+    this.rejbillers = this.billdata.filter((data)=>data['status']=='Rejected');
     if(this.pendingbillers==null){
       this.billerlength=0
     
@@ -57,8 +71,45 @@ export class DashboardMakerComponent implements OnInit {
       this.billerlength=this.pendingbillers.length;
       
     }
+    if(this.apprbillers==null){
+      this.apprbilllength=0
+    }else{
+      this.apprbilllength=this.apprbillers.length;
+    }
+    if(this.rejbillers==null){
+      this.rejbilllength=0
+    }else{
+      this.rejbilllength=this.rejbillers.length;
+    }
   }else{
     this.billerlength=0
+    this.apprbilllength=0
+    this.rejbilllength=0
+  }
+
+  if(this.paymentdata!=null){
+    this.penPaydata = this.paymentdata.filter((data)=>data['status']=='Pending');
+    this.apprPaydata = this.paymentdata.filter((data)=>data['status']=='Approved');
+    this.rejPayData = this.paymentdata.filter((data)=>data['status']=='Rejected');
+    if(this.penPaydata==null){
+      this.penpaylength=0
+    }else{
+      this.penpaylength=this.penPaydata.length; 
+    }
+    if(this.apprPaydata==null){
+      this.apprpaylength=0
+    }else{
+      this.apprpaylength=this.apprPaydata.length; 
+    }
+    if(this.rejPayData==null){
+      this.rejpaylength=0
+    }else{
+      this.rejpaylength=this.rejPayData.length; 
+    }
+  }else{
+this.penpaylength=0
+this.apprpaylength=0
+this.rejpaylength=0
   }
   }
 
