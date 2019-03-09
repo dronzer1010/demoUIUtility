5bbec81df9d5479f60e4ab29cbdc7fce59f7ade0
pratikbokey007 <pratikbokey007@gmail.com>
2019-03-09 17:35:51 +0530

    New pull request 09032019

--- src/app/routingcomponents/billerlist/billerlist.component.ts
+++ src/app/routingcomponents/billerlist/billerlist.component.ts
@@ -9,7 +9,7 @@ export class BillerlistComponent implements OnInit {
   display='none'; 
   billdata:any=[];
   billerlength:number=0;
-  noofrole="No billers available"
+  noofrole="No bills available"
   filterQuery = "";
   rowsOnPage = 300;
   sortBy = "email";
@@ -21,13 +21,13 @@ export class BillerlistComponent implements OnInit {
     console.log(this.billdata)
     if(this.billdata==null){
       this.billerlength=0
-      this.noofrole="No billers available"
+      this.noofrole="No bills available"
     }else{
       this.billerlength=this.billdata.length;
       if(this.billerlength>1){
-        this.noofrole="No of Billers:"
+        this.noofrole="No of Bills:"
       }else{
-        this.noofrole="No of Biller:"
+        this.noofrole="No of Bill:"
       }
     }
   }
