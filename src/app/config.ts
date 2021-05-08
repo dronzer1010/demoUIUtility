8b4c1cadea7942b0df183ec2058c18e40908431b
dipanshu0991 <deepss507@gmail.com>
2021-05-08 23:17:08 +0530

    New Authcode Setup

--- src/app/config.ts
+++ src/app/config.ts
@@ -9,27 +9,27 @@ export class Config{
 
     //Billtree Java End Point
     //this.baseUrl="http://4239837fb792.ngrok.io/api/" //Ngrok
-    this.baseUrl="https://backtest-billtree.aquapay.in/api/" //Test
-    //this.baseUrl = "https://utility-rest.aquapay.in/api/" //Prod
+    //this.baseUrl="https://backtest-billtree.aquapay.in/api/" //Test
+    this.baseUrl = "https://utility-rest.aquapay.in/api/" //Prod
     //this.baseUrl="http://localhost:8080/api/" //localhost
 
     //Billtree Node End Point
-    //this.utilityapiUrl = "https://billtree.aquapay.in:3002/"; //prod
-    this.utilityapiUrl="https://utility-test.aquapay.in:9999/" //Test
+    this.utilityapiUrl = "https://billtree.aquapay.in:3002/"; //prod
+    //this.utilityapiUrl="https://utility-test.aquapay.in:9999/" //Test
     //this.utilityapiUrl="http://baedaf71d248.ngrok.io/" //ngrok
     
     
 
     //Billtree Java End Point Back
     //this.backurl="http://71f2b37904bb.ngrok.io" //ngrok
-    this.backurl="https://backtest-billtree.aquapay.in" //test
-    //this.backurl="https://utility-rest.aquapay.in" //live
+    //this.backurl="https://backtest-billtree.aquapay.in" //test
+    this.backurl="https://utility-rest.aquapay.in" //live
     //this.loginurl="http://9f96f8bcd809.ngrok.io/" //ngrok
 
 
     //Single Sign on End Point
-    this.loginurl="https://test-login.aquapay.in:4000/"//test
-    //this.loginurl="https://single-signon.aquapay.in:4000/"//live
+    //this.loginurl="https://test-login.aquapay.in:4000/"//test
+    this.loginurl="https://single-signon.aquapay.in:4000/"//live
 
 
     //this.gravitaorgid=339;//Test
--- src/app/urlconfig.ts
+++ src/app/urlconfig.ts
@@ -5,17 +5,17 @@ export class Urlconfig{
 
     constructor(){
         //this.billtreeurl="http://localhost:4100/#/" //Local
-        this.billtreeurl="https://utility-test.aquapay.in/#/"  //Test
-        //this.billtreeurl="https://billtree.aquapay.in/#/" //Production
+        //this.billtreeurl="https://utility-test.aquapay.in/#/"  //Test
+        this.billtreeurl="https://billtree.aquapay.in/#/" //Production
 
         //this.gsturl="http://localhost:4000/" //Local
-        this.gsturl="https://test-gst.aquapay.in/" //Test
-        //this.gsturl="https://gst-payments.aquapay.in/" //Production
+        //this.gsturl="https://test-gst.aquapay.in/" //Test
+        this.gsturl="https://gst-payments.aquapay.in/" //Production
 
 
        //this.supplierurl="http://localhost:4200/" //Local
-       this.supplierurl="https://test-supplier.aquapay.in/" //Test
-       //this.supplierurl="https://supplier-payments.aquapay.in/" //Production
+       //this.supplierurl="https://test-supplier.aquapay.in/" //Test
+       this.supplierurl="https://supplier-payments.aquapay.in/" //Production
         
 
     }
